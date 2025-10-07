148bf4858f5957636e50ee3f8e23712fddf55590
Luke Bollwerk <lukas8@gmail.com>
2025-10-07 07:27:56 -0600

    respond to media events

--- app/src/commonMain/kotlin/streetlight/app/ui/LiveEventView.kt
+++ app/src/commonMain/kotlin/streetlight/app/ui/LiveEventView.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.datetime.Instant
 import pondui.ui.controls.*
 import pondui.ui.modifiers.MagicItem
+import pondui.ui.services.MediaEventEffect
 import streetlight.model.data.*
 
 @Composable
@@ -19,6 +20,10 @@ fun LiveEventView(
     viewModel: LiveEventModel = viewModel { LiveEventModel(eventId) }
 ) {
     val state by viewModel.stateFlow.collectAsState()
+
+    MediaEventEffect {
+        println("Squawk! $it")
+    }
 
     Column(1) {
         EventStatusDash(
