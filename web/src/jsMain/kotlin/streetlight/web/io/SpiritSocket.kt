adce0aa513689cdb8294c87f41e2a6e7a98c4217
bonzaiferroni <lukas8@gmail.com>
2026-03-03 10:01:10 -0700

    add SpiritSocket (Junie)

---
+++ web/src/jsMain/kotlin/streetlight/web/io/SpiritSocket.kt
@@ -0,0 +1,51 @@
+package streetlight.web.io
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.w3c.dom.WebSocket
+import streetlight.model.data.SpiritDelta
+
+class SpiritSocket(
+    private val socket: WebSocket,
+    private val scope: CoroutineScope
+) {
+    private val _deltaFlow = MutableSharedFlow<SpiritDelta>(
+        replay = 20,
+        extraBufferCapacity = 64
+    )
+    val deltaFlow: Flow<SpiritDelta> = _deltaFlow
+
+    init {
+        socket.onmessage = { event ->
+            scope.launch {
+                val data = event.data
+                if (data is String) {
+                    val delta = data.decode()
+                    if (delta != null) {
+                        _deltaFlow.emit(delta)
+                    } else {
+                        console.log("invalid spirit delta: $data")
+                    }
+                }
+            }
+        }
+    }
+
+    fun send(delta: SpiritDelta) {
+        socket.send(delta.encode())
+    }
+}
+
+private fun SpiritDelta.encode() = Json.encodeToString(this)
+
+private fun String.decode(): SpiritDelta? {
+    return try {
+        Json.decodeFromString(this)
+    } catch (e: Exception) {
+        null
+    }
+}
