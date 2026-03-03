58b1fd74b2abb90856ca771000c21e43501b0b7e
bonzaiferroni <lukas8@gmail.com>
2026-03-03 09:37:15 -0700

    add SpiritConnection (Junie)

--- model/src/commonMain/kotlin/streetlight/model/data/Spirit.kt
+++ model/src/commonMain/kotlin/streetlight/model/data/Spirit.kt
@@ -2,8 +2,10 @@ package streetlight.model.data
 
 import kampfire.model.GeoPoint
 import kampfire.utils.randomUuidString
+import kotlinx.serialization.Serializable
 import kotlin.jvm.JvmInline
 
+@Serializable
 data class Spirit(
     val spiritId: SpiritId,
     val geoPoint: GeoPoint,
@@ -11,9 +13,16 @@ data class Spirit(
 ) {
 }
 
+@Serializable
 @JvmInline
 value class SpiritId(val value: String) {
     companion object {
         fun random() = SpiritId(randomUuidString())
     }
 }
+
+@Serializable
+sealed interface SpiritDelta
+
+@Serializable
+data class SpiritPointDelta(val spiritId: SpiritId, val geoPoint: GeoPoint) : SpiritDelta
